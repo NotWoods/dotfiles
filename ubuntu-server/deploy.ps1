07df2836c41ccd21866cecb2304cc77ece152fd8
Tiger Oakes <contact@tigeroakes.com>
2020-07-02 12:19:14 -0700

    Add homer config

--- ubuntu-server/deploy.ps1
+++ ubuntu-server/deploy.ps1
@@ -1,4 +1,10 @@
 $Server = Read-Host -Prompt 'Where to deploy to? (user@hostname)'
+$IP = $Server.Split("@")[1]
+
+$RawHomerConfig = Get-Content -Path './homer/config.yml'
+$RawHomerConfig.Replace('<ipaddr>', $IP) | Set-Content -Path './homer/temp.yml'
+scp ./homer/temp.yml ${Server}:~/.homer/config.yml
+Remove-Item ./homer/temp.yml
 
 scp ./docker-compose.yml ${Server}:~/docker-compose.yml
 scp ./Caddyfile ${Server}:~/.caddy/Caddyfile
