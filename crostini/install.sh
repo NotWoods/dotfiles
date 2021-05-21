383bda15286ecd938a4820abc5d47077e913bcfb
Tiger Oakes <contact@tigeroakes.com>
2021-05-21 10:51:09 -1000

    Add node

--- crostini/install.sh
+++ crostini/install.sh
@@ -2,8 +2,14 @@
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
+# Node v16.x
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
 sudo apt update
-sudo apt install gh nano
+sudo apt install gh nano nodejs
 
 # code-server
 curl -fsSL https://code-server.dev/install.sh | sh
+
+# pnpm
+npm i -g pnpm
